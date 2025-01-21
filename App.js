087d391c4d98f9ce6529de1967c3b9824821f0c0
bukwon <dnu05043@gmail.com>
2025-01-21 22:06:50 +0900

    control status bar

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
   const [number, setNumber] = useState(0);
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 50 }}>결과 : {number}</Text>
-      <View style={{ flexDirection: 'row', gap: 10 }}>
-        <Button title="더하기" onPress={() => setNumber(number + 1)} />
-        <Button title="빼기" onPress={() => setNumber(number - 1)} />
-      </View>
-      <StatusBar style="auto" />
+      <Text style={{ color: 'blue' }}>리액트 네이티브</Text>
+      <Text style={styles.text}>리액트 네이티브</Text>
+      <StatusBar
+        backgroundColor='red'/>
     </View>
   );
 }
@@ -20,8 +18,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    color: 'blue',
   },
 });
