25c68f0324cec0135741177754beb72dec970627
bukwon <dnu05043@gmail.com>
2025-02-12 22:07:31 +0900

    lat, lon 기반으로 현재 주소 가져오기

--- App.js
+++ App.js
@@ -1,17 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Dimensions } from 'react-native';
+
+import * as Location from 'expo-location';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 console.log(SCREEN_WIDTH);
 
 const App = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [city, setCity] = useState(null);
+
+  // 허가 여부
+  const [permitted, setPermitted] = useState(true);
+
+  const locationData = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    // console.log(permission);
+
+    if (!granted) {
+      setPermitted(false);
+      setErrorMsg('위치에 대한 권한 부여가 거부되었습니다.');
+
+      return;
+    }
+
+    const {coords: {latitude ,longitude}} = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    
+
+    const address = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false });
+
+      // console.log(address);
+      // console.log(address[0].city);
+
+      const cityAddress = address[0].city;
+      setCity(cityAddress);
+  };
+
+
+  useEffect(() => {
+    locationData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.cityCon}>
-        <Text style={styles.city}>Ansan</Text>
+        <Text style={styles.city}>{city}</Text>
       </View>
       <View style={styles.regDateCon}>
         <Text style={styles.regDate}>10월 13일, 일, 13:18</Text>
